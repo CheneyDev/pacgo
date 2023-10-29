fd2b82cda12d262465cdb65eb2f2fc74c1ececd2
CheneyDev <cheneydev@gmail.com>
2023-10-29 17:34:49 +0800

    reading from Stdin

--- main.go
+++ main.go
@@ -10,26 +10,6 @@ import (
 
 var maze []string
 
-func initialise()  {
-	cbTerm:=exec.Command("stty","cbreak","-echo")
-	cbTerm.Stdin=os.Stdin
-
-	err:=cbTerm.Run()
-	if err != nil {
-		log.Fatalln("unable to active cbreak mode: "err)
-	}
-}
-
-func cleanup()  {
-	cookedTerm:=exec.Command("stty","-cbreak","echo")
-	cookedTerm.Stdin=os.Stdin
-
-	err:=cookedTerm.Run()
-	if err != nil {
-		log.Fatalln("unable to restore cooked mode: "err)
-	}
-}
-
 func loadMaze(file string) error {
 	f, err := os.Open(file)
 	if err != nil {
@@ -53,6 +33,9 @@ func printScreen() {
 
 func main() {
 
+	initialise()
+	defer cleanup()
+
 	//load resources
 	err := loadMaze("maze01.txt")
 	if err != nil {
@@ -66,4 +49,39 @@ func main() {
 
 		break
 	}
+}
+
+func initialise() {
+	cbTerm := exec.Command("stty", "cbreak", "-echo")
+	cbTerm.Stdin = os.Stdin
+
+	err := cbTerm.Run()
+	if err != nil {
+		log.Fatalln("unable to active cbreak mode: ", err)
+	}
+}
+
+func cleanup() {
+	cookedTerm := exec.Command("stty", "-cbreak", "echo")
+	cookedTerm.Stdin = os.Stdin
+
+	err := cookedTerm.Run()
+	if err != nil {
+		log.Fatalln("unable to restore cooked mode: ", err)
+	}
+}
+
+func readInput() (string, error) {
+	buffer := make([]byte, 100)
+
+	cnt, err := os.Stdin.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+
+	if cnt == 1 && buffer[0] == 0x1b {
+		return "ESC", nil
+	}
+
+	return "", nil
 }
