484b0cc40032a1d22891c7174c3b613b6901e01b
CheneyDev <cheneydev@gmail.com>
2023-10-29 21:31:57 +0800

    updating the maze

--- main.go
+++ main.go
@@ -46,8 +46,21 @@ func loadMaze(file string) error {
 func printScreen() {
 	simpleansi.ClearScreen()
 	for _, line := range maze {
-		fmt.Println(line)
+		for _, chr := range line {
+			switch chr {
+			case '#':
+				fmt.Printf("%c", chr)
+			default:
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
 	}
+
+	simpleansi.MoveCursor(player.row, player.col)
+	fmt.Print('P')
+
+	simpleansi.MoveCursor(len(maze)+1, 0)
 }
 
 func readInput() (string, error) {
