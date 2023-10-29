f0e19c48ba3a817e67f46994f01b4d35e4a96198
CheneyDev <cheneydev@gmail.com>
2023-10-29 15:08:55 +0800

    load the maze file

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+var maze []string
+
+func loadMaze(file string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		maze = append(maze, line)
+	}
+	return nil
+}
